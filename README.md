2c7b8b5d9c61c38db367f42ba90e7404bda7921e
thercg <revivalcoin@gmail.com>
2016-01-07 22:24:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Elacoin - an elastic cryptocurrency
+===================================
+
+Hardfork Notice: Elacoin will switch to Scrypt-N Adaptive and KGW at September 24th, 2014 00:00:00 GMT!
+
+Elastic block reward algorithm based on the difficulty and Moore's Law
+The more computational power you throw at it, the more each block will give. 
+The reward is halved every nine months to preserve the coin's value.
+
+Stable Currency that Regulates Itself Through Supply and Demand
+Less demand? Less hashpower, less block reward, less supply. Same value.
+High demand? More hashpower, more block reward, more supply. Same value.
+
+Fast Payments and Confirmations
+The block target is one every 2 minutes, which is the perfect amount.
+Fast enough without running into network IO limits, or bloating your hard drive.
+
+Open to everyone. No premining, no shenanigans, announced release
+Elacoin will be released after 24 hours of this message. Elacoin is not premined,
+with the genesis block timestamping "Traffic from Syria Disappears from Internet".
---
+++ src/checkpoints.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2016 TheRCG Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+#include "checkpoints.h"
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+        (     0, uint256("0x521d2bc9fdb2534f26e5df423db7a64da14892993e4b4f0329c829a5e8194305"))
+        (  2500, uint256("0xd301c065649cbbfb1e93cec353047a12256365c9d50ac689b23ad913df25b6fd"))
+        (  5000, uint256("0x7671812122b751155ca6ed038f7b48e0f4f98a27a40dc9521d1c61d98e587544"))
+        ( 19200, uint256("0x0d0ce80dfac71b50c597a72bc4ca23f4c8ced8e6c3276716a01ba9c80b353c1d"))
+        ( 19350, uint256("0xc6c725fb3c376a2cc4a0796ab4aa04af1ff472628c8e82ece55657b60b3150a5"))
+        ( 19400, uint256("0xce70abf9c0930d5cdcff555ceec6e6777f70f3a8bbc4b1efc9ac5c49a3b6c8e6"))
+        (240000, uint256("0x36ef90af57e5fa454fa8206c577d6c902b9a3791bdc3f9651d8d08fc03b88995"))
+        (300000, uint256("0x3548b677ea55e70e2c9b8c6384ef884388a42c175cf9a65f99b420f31e762515"))
+        (315000, uint256("0xb3b3978ae8be494594bda72dc5409ab1c646411d68820f5716f2ff4148d36941"))
+        (464200, uint256("0xb2f7cfe855dee276d250f06718edd29b14f285eeefd5a61ba379ff8e47bcccd7"))
+    ;
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+
+        return mapCheckpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+}
